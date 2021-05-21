4ec182637ffc05e8f242a288272a6b616880e2b9
Nick (nift4) <nift4@protonmail.com>
2021-05-21 08:32:04 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,6 +5,8 @@ export my_dir=$(pwd)
 echo "Loading configuration..."
 source "${my_dir}"/config.sh
 
+echo "CLEAN build!"
+
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "Please set GITHUB_TOKEN before continuing."
     exit 1
